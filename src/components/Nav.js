3cc7e6d5ac47dea5c439128503f409588d97d8b2
Savitender Singh <paramsavi06@gmail.com>
2024-05-29 14:00:40 +0530

    Changed css

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,11 +3,11 @@ import '../styles/Nav.css';
 const Nav = () => (
   <section className="nav-1">
     <nav>
-      <a className="item" href="#nav-1"><div className="linktext">Home</div></a>
+      <a className="item" href="#home"><div className="linktext">Home</div></a>
       <a className="item" href="#nav-1"><div className="linktext">About</div></a>
-      <a className="item" href="#nav-1"><div className="linktext">Portfolio</div></a>
+      <a className="item" href="#projects"><div className="linktext">Projects</div></a>
+      <a className="item" href="#certifications"><div className="linktext">Certifications</div></a>
       <a className="item" href="#nav-1"><div className="linktext">Contact</div></a>
-      <a className="item" href="#nav-1"><div className="linktext">Blog</div></a>
     </nav>
   </section>
 );
